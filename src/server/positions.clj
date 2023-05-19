7cdecf20323973c2d5a847f8e6625d407aa0c33f
fcadet <florian.kd@gmail.com>
2023-05-19 17:15:14 +0100

    latest

--- src/server/positions.clj
+++ src/server/positions.clj
@@ -7,7 +7,9 @@
   )
 
 ;TODO handle cash, remove from ticker list for market data and add back after
-
+;-------------------------------------------------------------------------------------------------------------------------------------------
+;---------------------------------------------------------------GET POSITIONS---------------------------------------------------------------
+;-------------------------------------------------------------------------------------------------------------------------------------------
 (defn get-clean-positions []
   (let [raw-positions (t/import-txt "positions.csv")
         yahoo-snapshot-data (group-by :ticker (mdata/get-yahoo-snapshot-data (map :ticker raw-positions)))
@@ -21,7 +23,7 @@
         positions-clean (->>  positions-with-market-data
                               (map #(assoc % :quantity (read-string (:quantity %))))
                               (map #(assoc % :cost-per-unit (read-string (:cost-per-unit %))))
-                              (map #(assoc % :nb-strategies  1))
+                              (map #(assoc % :nb-strategies (count (remove empty? [(:strategy-1 %) (:strategy-2 %) (:strategy-3 %)]))))
 
                               (map #(assoc % :cost-value-local (* (:quantity %) (:cost-per-unit %))))
                               (map #(assoc % :nav-local (* (:quantity %) (:regularMarketPrice %))))
@@ -37,6 +39,7 @@
                            [strat (count (filter #(= % strat) (concat (map :strategy-3 positions-clean) (map :strategy-2 positions-clean) (map :strategy-1 positions-clean))))]))
         positions-alloc (->>  positions-clean
                               (map #(assoc % :nav-eur-perc (/ (:nav-eur %) nav-eur )))
+                              (map #(assoc % :nav-eur-perc-per-strat (/ (:nav-eur-perc %) (:nb-strategies %) )))
                               (map #(assoc % :alloc-strat-1 (if (= (:strategy-1 %) "") 0 (/ (* nav-eur (/ (static/allocation-model (:strategy-1 %)) 100)) (allocation-count (:strategy-1 %))))))
                               (map #(assoc % :alloc-strat-2 (if (= (:strategy-2 %) "") 0 (/ (* nav-eur (/ (static/allocation-model (:strategy-2 %)) 100 )) (allocation-count (:strategy-2 %))))))
                               (map #(assoc % :alloc-strat-3 (if (= (:strategy-3 %) "") 0 (/ (* nav-eur (/ (static/allocation-model (:strategy-3 %)) 100 )) (allocation-count (:strategy-3 %))))))
@@ -46,29 +49,25 @@
     positions-alloc
     )
   )
+;-------------------------------------------------------------------------------------------------------------------------------------------
+;---------------------------------------------------------------GET ANALYTICS---------------------------------------------------------------
+;-------------------------------------------------------------------------------------------------------------------------------------------
+(defn get-top-10 [clean-positions] (map #(select-keys % [:nav-eur-perc :nav-eur :ticker]) (take 10 (reverse (sort-by :nav-eur-perc clean-positions)))))
+(defn get-characteristics [clean-positions] [{:metric "nav-eur" :value (reduce + (map :nav-eur clean-positions))}
+                                             {:metric "count-x-cash" :value (count (t/chainfilter {:strategy-1 #(not (= % "CASH"))} clean-positions))}
+                                             {:metric "count-etf" :value (count (t/chainfilter {:asset-class #(= % "ETF")} clean-positions))}
+                                             {:metric "top10-weight" :value (reduce + (map :nav-eur-perc (take 10 (reverse (sort-by :nav-eur-perc clean-positions)))))}
+                                             {:metric "cash" :value (reduce + (map :nav-eur-perc (t/chainfilter {:strategy-1 #(= % "CASH")} clean-positions)))}
+                                             {:metric "div" :value (reduce + (map #(* (:nav-eur-perc %) (if (nil? (:dividendYield %)) 0.0 (:dividendYield %))) clean-positions))}
+                                             {:metric "fpe" :value (reduce + (map #(* (:nav-eur-perc %) (if (nil? (:forwardPE %)) 15 (:forwardPE %))) clean-positions))}
+                                             {:metric "pb" :value (reduce + (map #(* (:nav-eur-perc %) (if (nil? (:priceToBook %)) 3 (:priceToBook %))) clean-positions))}
+                                             {:metric "beta" :value (reduce + (map #(* (:nav-eur-perc %) (if (nil? (:beta %)) 1 (:beta %))) clean-positions))}])
 
-;(get top-10 )
-;...
-
-
-(defn get-clean-positions-with-analytics []
-  (let [clean-positions (get-clean-positions)
-        top10 (map #(select-keys % [:nav-eur-perc :nav-eur :ticker]) (take 10 (reverse (sort-by :nav-eur-perc clean-positions))))
-        characteristics [{:metric "nav-eur" :value (reduce + (map :nav-eur clean-positions))}
-                         {:metric "count-x-cash" :value (count (t/chainfilter {:strategy-1 #(not (= % "CASH"))} clean-positions))}
-                         {:metric "count-etf" :value (count (t/chainfilter {:asset-class #(= % "ETF")} clean-positions))}
-                         {:metric "top10-weight" :value (reduce + (map :nav-eur-perc top10))}
-                         {:metric "cash" :value (reduce + (map :nav-eur-perc (t/chainfilter {:strategy-1 #(= % "CASH")} clean-positions)))}
-                         {:metric "div" :value (reduce + (map #(* (:nav-eur-perc %) (:dividendYield %)) clean-positions))}
-                         {:metric "fpe" :value (reduce + (map #(* (:nav-eur-perc %) (:forwardPE %)) clean-positions))}
-                         {:metric "pb" :value (reduce + (map #(* (:nav-eur-perc %) (:priceToBook %)) clean-positions))}
-                         {:metric "beta" :value (reduce + (map #(* (:nav-eur-perc %) (:beta %)) clean-positions))}
-                         ]
-        exposure-strategy-1 (for [strat (group-by :strategy-1 clean-positions)] {:strategy-1 (key strat) :nav-eur-perc (reduce + (map :nav-eur-perc (val strat)))})
-        ;TODO allocation total strat 123, kinda weight / count of strat, for each strat
-        ]
-    [clean-positions top10 characteristics exposure-strategy-1]
-    ))
+(defn get-strategy-exposure [clean-positions]
+(for [strat (distinct (map :strategy-1 clean-positions))]
+  {:strategy strat :nav-eur-perc (+ (reduce + (map :nav-eur-perc-per-strat (t/chainfilter {:strategy-1 #(= % strat)} clean-positions)))
+                                    (reduce + (map :nav-eur-perc-per-strat (t/chainfilter {:strategy-2 #(= % strat)} clean-positions)))
+                                    (reduce + (map :nav-eur-perc-per-strat (t/chainfilter {:strategy-3 #(= % strat)} clean-positions))))}))
 
 ;-----------------------------------------------all needed---------------
 
