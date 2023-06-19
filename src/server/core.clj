168e48dc3d541bc43a6f9af59fd30a48b7da06c2
fcadet <florian.kd@gmail.com>
2023-06-19 17:40:51 +0100

    latest

--- src/server/core.clj
+++ src/server/core.clj
@@ -5,6 +5,8 @@
             )
   )
 
+;TODO insert (-main)  that trigger all need fct ? fx first and then rest ?
+
 (defn fx-data [] (marketdata/get-yahoo-last-price static/list-fx))
 (defn metals-data [] (marketdata/get-yahoo-last-price static/list-metals))
 ;(defn snapshot-data [] (marketdata/get-yahoo-snapshot-data static/list-tickers))
